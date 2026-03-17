1d881280676c4b35ad6ded9413fdd9edaed68a0c
EuniceA-tech8 <eunice.aduagyei01@gmail.com>
2026-03-17 18:01:47 -0400

    Add files via upload

---
+++ ImageGallery/CustomImage.swift
@@ -0,0 +1,32 @@
+//
+//  CustomImage.swift
+//  ImageGallery
+//
+//  Created by BMCC Lab Guest on 3/17/26.
+//
+
+import SwiftUI
+
+struct CustomImage: View {
+    //create variables that will be used for our custom image view
+    var imageName: String
+    var borderSize: CGFloat
+    var borderColor: Color
+    var shadowRadius: CGFloat
+    
+    
+    var body: some View {
+        Image(imageName)
+        // resizable lets you change the dimensions of an image
+            .resizable()
+            .scaledToFit()
+            .aspectRatio(contentMode: .fit)
+            .shadow(radius: shadowRadius)
+            .padding()
+      
+    }
+}
+
+#Preview {
+    CustomImage(imageName:"beach.png", borderSize: 10, borderColor: .blue, shadowRadius: 20)
+}
---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by BMCC Lab Guest on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
